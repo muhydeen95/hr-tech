159637b86dfd58068225b7c4be559da9e9c738cb
biinbelllo <abdulmateenbinbello@gmail.com>
2022-12-21 22:59:17 +0100

    fix: add fmtlive config and env file

---
+++ src/environments/environment.fmtlive.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  api_url: 'https://filetraliveapiv1.azurewebsites.net/api/',
+  cloudUrl: 'https://login.microsoftonline.com',
+  scopeUri: [
+    'api://9eba0b88-c9e1-4471-841e-5191a80fe014/Access_DocStream_As_A_User',
+  ],
+  tenantId: 'd75facc4-6467-47d9-ac73-f8fcb4ba0845',
+  clienId: '6d045903-d5fe-40ad-82b5-c74b9dab40e6',
+  domainUrl: 'docstreamdev.onmicrosoft.com',
+  authRedirectUrl: '/login/validate-user',
+};
